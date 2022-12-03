04ddf43196fe3969a79bac991e5a31e1f773af56
JFEstevezC <jose2224646@correo.uis.edu.co>
2022-12-03 06:11:01 -0500

    Avances del proyecto

--- j.py
+++
@@ -1,9 +0,0 @@
-import time
-
-tiempo = int(input('¿Cuánto tiempo deseas que dure el temporizador (en segundos)? '))
-
-for x in range(tiempo):
-    print(tiempo - x)
-    time.sleep(1)
-
-print('¡El tiempo ha terminado!')
